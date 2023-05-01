07ae0c1b995f37d47a8a8f3cbf9e54c7f771a511
bue <epbucher@iu.edu>
2023-05-01 11:53:44 -0400

    @ brca_hypoxia_cc3d : get ready for implementation.

--- Simulation/BC_Hypoxia.py
+++ Simulation/BC_Hypoxia.py
@@ -1,10 +1,9 @@
-
+# libararies
 from cc3d import CompuCellSetup
-        
-
 from BC_HypoxiaSteppables import BC_HypoxiaSteppable
 
+# register steppables
 CompuCellSetup.register_steppable(steppable=BC_HypoxiaSteppable(frequency=1))
 
-
+# run cc3d
 CompuCellSetup.run()
--- Simulation/BC_HypoxiaSteppables.py
+++ Simulation/BC_HypoxiaSteppables.py
@@ -1,34 +1,35 @@
 from cc3d.core.PySteppables import *
 import numpy as np
 
+
 class BC_HypoxiaSteppable(SteppableBasePy):
 
     def __init__(self, frequency=1):
-
         SteppableBasePy.__init__(self,frequency)
 
     def start(self):
         """
         Called before MCS=0 while building the initial simulation
         """
+        pass
 
     def step(self, mcs):
         """
         Called every frequency MCS while executing the simulation
-        
         :param mcs: current Monte Carlo step
         """
-
         for cell in self.cell_list:
-
             print("cell.id=",cell.id)
 
     def finish(self):
         """
         Called after the last MCS to wrap up the simulation
         """
+        pass
 
     def on_stop(self):
         """
         Called if the simulation is stopped before the last MCS
         """
+        pass
+
