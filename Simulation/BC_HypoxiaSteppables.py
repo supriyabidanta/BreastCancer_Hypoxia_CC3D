2a1e8aacdfefbe9bcb5675ac9356246051e1a00c
Supriya Bidanta <sbidanta@iu.edu>
2023-05-03 17:06:54 -0400

    further

--- Simulation/BC_HypoxiaSteppables.py
+++ Simulation/BC_HypoxiaSteppables.py
@@ -2,6 +2,9 @@ from cc3d.core.PySteppables import *
 
 import numpy as np
 
+r_sigma_s = 38.0# saturation
+r_sigma_h = 10.0 #hypoxia
+r_sigma_t = 6.0 #proliferation
 
 class BC_HypoxiaSteppable(SteppableBasePy):
 
@@ -15,11 +18,21 @@ class BC_HypoxiaSteppable(SteppableBasePy):
         for cell in self.cell_list:
             cell.targetVolume = 46  # ((2492 [um**3])**(1/3) / 2[px/um])**(2) = 45.952[px**2]
             cell.lambdaVolume = 4
+            cell.dict['hypoxia'] = 0
 
     def step(self, mcs):
-        secretor = self.get_field_secretor ("oxygen")
-        for cell in self.cell_list:
-            secretor.uptakeInsideCell(cell, 0.05,0.0025)
+        field=self.getConcentrationField('oxygen')
+        for cell in self.cellList:
+            # check if cell in hypoxia condition
+            r_o2 = field[cell.xCOM, cell.yCOM, cell.zCOM]
+            
+            if (r_o2 < r_sigma_t):  # proliferation
+                cell.type = self.Necrotic
+            if (r_o2 < r_sigma_h):  # hypoxia
+                cell.type = self.GFP
+            if (r_o2 < r_sigma_s):  # saturation
+                cell.type = self.DsRed
+            
 
     def finish(self):
         """
